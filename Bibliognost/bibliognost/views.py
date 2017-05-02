e221d0412d0373289652b9cdeee3ee5b4f86fe43
tushar117 <tushar.abhishek@ymail.com>
2017-05-02 16:18:58 +0530

    modify the book meta route (#5)
    * Add Book Search
    
    * Add missing import for Book Search
    
    * resolve lxml
    
    * remove pycache files
    
    * Add multinomial naive bayes classifier
    
    * modify requirements.txt
    
    * modify gitignore
    
    * modify the book meta route

--- Bibliognost/bibliognost/views.py
+++ Bibliognost/bibliognost/views.py
@@ -1,6 +1,5 @@
 import itertools
 import multiprocessing
-import time
 
 from flask import jsonify, request
 
@@ -13,7 +12,7 @@ from ..modules.sent_analysis import fastClassifier
 logger = get_logger(__file__)
 
 
-@biblio.route('/')
+@biblio.route('/book-meta')
 def index():
 	book = GoodReads(request.args.get('book_id'))
 	return jsonify(book.get_book_data())
@@ -48,20 +47,15 @@ def goodreads_reviews(url):
 @biblio.route('/reviews')
 def reviews_with_sentiment():
 	pool = multiprocessing.Pool()
-	t0 = time.time()
 	fetch_review_processes = [
 		pool.apply_async(amazon_reviews, args=(request.args.get('isbn'),)),
 		pool.apply_async(goodreads_reviews, args=(request.args.get('url'),))
 	]
 	amzn_reviews, gr_reviews = [reviews.get() for reviews in fetch_review_processes]
-	print('time taken: ', (time.time() - t0))
 	review_texts = [review.get('body') for review in itertools.chain(amzn_reviews, gr_reviews)]
-	print('num reviews ', len(review_texts))
-	t1 = time.time()
 	sentiments = []
 	for review in review_texts:
 		sentiments.append(fastClassifier.predict_sentiment(review)[0])
-	print('time taken: ', (time.time() - t1))
 	for idx, response in enumerate(itertools.chain(amzn_reviews, gr_reviews)):
 		response['sentiment'] = int(sentiments[idx])
 	return jsonify({'amazon': amzn_reviews, 'goodreads': gr_reviews})
